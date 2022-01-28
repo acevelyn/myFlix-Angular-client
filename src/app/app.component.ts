30bae045e16e1fb956c68ba410d0b191476cfd10
acevelyn <86307435+acevelyn@users.noreply.github.com>
2022-01-28 15:25:46 -0500

    Transferred app.ts code to welcome-page.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -13,21 +9,4 @@ import { MatDialog } from '@angular/material/dialog';
 export class AppComponent {
   title = 'FlixAngularClient';
 
-  constructor(public dialog: MatDialog) { }
-
-  // Function that will open the sign up dialog when the sign up button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
-  }
-
-  // Function that will open the login dialog when the login button is clicked
-  openLoginForm(): void {
-    this.dialog.open(UserLoginFormComponent, { width: '280px' })
-  }
-
-  // Function that will open the movie cards dialog when the all movies button is clicked
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, { width: '500px' })
-  }
-
-} // end of class
+}
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-welcome-page',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  // Function that will open the sign up dialog when the sign up button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
+  }
+
+  // Function that will open the login dialog when the login button is clicked
+  openLoginForm(): void {
+    this.dialog.open(UserLoginFormComponent, { width: '280px' })
   }
 
 }

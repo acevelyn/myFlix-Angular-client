3738a0b92570e11e565866a53574ddb16c6a859d
acevelyn <86307435+acevelyn@users.noreply.github.com>
2022-01-27 19:03:43 -0500

    Update user-login-form.component.ts

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-login-form',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() userCredentials = { Username: '', Password: '' }
 
-  ngOnInit(): void {
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void { }
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userCredentials).subscribe(
+      (result) => {
+        // set user and token to local storage
+        localStorage.setItem('token', result.token);
+        localStorage.setItem('user', JSON.stringify(result.user));
+        // log the result
+        console.log(result);
+        this.dialogRef.close();
+
+        this.snackBar.open('Successfully Logged In!', 'OK', {
+          duration: 2000,
+        });
+
+      }, (result) => {
+        console.log(result);
+        this.snackBar.open('Invalid Credentials. Please try again.', 'OK', {
+          duration: 2000,
+        });
+      });
   }
 
 }

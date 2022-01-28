c653e17beab6f3d634d6a08d77942444c0011a5b
acevelyn <86307435+acevelyn@users.noreply.github.com>
2022-01-28 15:06:02 -0500

    Added All Movies Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -14,13 +15,19 @@ export class AppComponent {
 
   constructor(public dialog: MatDialog) { }
 
-  // This is the function that will open the dialog when the sign up button is clicked
+  // Function that will open the sign up dialog when the sign up button is clicked
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
   }
 
+  // Function that will open the login dialog when the login button is clicked
   openLoginForm(): void {
     this.dialog.open(UserLoginFormComponent, { width: '280px' })
+  }
+
+  // Function that will open the movie cards dialog when the all movies button is clicked
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, { width: '500px' })
   }
 
 } // end of class
